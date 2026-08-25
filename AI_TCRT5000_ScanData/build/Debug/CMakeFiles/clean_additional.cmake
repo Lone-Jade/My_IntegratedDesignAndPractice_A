33a8c5a020faaf45dd987e19048dddc753d002f5
Lone-Jade <1482143652@qq.com>
2026-08-26 00:22:49 +0800

    重命名工程为 AI_TCRT5000_ScanData：更新工程名与构建/调试配置，移除旧构建产物

--- AI_TCRT5000_ScanData/build/Debug/CMakeFiles/clean_additional.cmake
+++
@@ -1,8 +0,0 @@
-# Additional clean files
-cmake_minimum_required(VERSION 3.16)
-
-if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "Debug")
-  file(REMOVE_RECURSE
-  "AI_TRY_TCRT500.map"
-  )
-endif()
